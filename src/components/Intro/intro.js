15be4a2bd54bfdcbac84ee75df1ddb56c747e27a
bishakhabharti <thebishakhabharti@gmail.com>
2025-11-06 14:32:12 +0530

    added new files

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './intro.css';
+import bg from '../../assets/bishu logo.png';
+import {Link} from 'react-scroll';
+import btnImg from '../../assets/hireme.png';
+
+const Intro = () => {
+  return (
+  <section id="intro">
+    <div className='introContent'>
+        <span className='hello'>Hello,</span>
+        <span className='introText'>I'm <span className='introName'>Bishakha</span><br />Website Designer</span>
+        <p className='introPara'>I am a skilled web designer with experience in creating<br /> visually appealing and user-frinedly websites.</p>
+        <Link><button className='btn'><img src={btnImg} alt='Hire' className='btnImg' />Hire Me</button></Link>
+    </div>
+    <img src={bg} alt='Profile' className='bg' />
+     
+    
+  </section>
+  );
+}
+
+export default Intro;
---
+++ src/components/Skills/skills.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png';
+
+
+const Skills = () => {
+  return (
+   <section id='skills'>
+    <span className='skillTitle'>What I do</span>
+    <span className='skillDesc'>Highly motivated Full Stack Developer and aspiring Program Analyst with experience in building and optimizing web applications.</span>
+    <div className='skillBars'>
+        <div className='skillBar'>
+            <img src={UIDesign} alt='UIDesign' className='skillBarImg'/>
+            <div className='skillBarText'>
+                <h2>UI/UX Design</h2>
+                <p>This is a demo text, you can write your own content here.</p>
+            </div>
+</div>
+             <div className='skillBar'>
+            <img src={WebDesign} alt='WebDesign' className='skillBarImg'/>
+            <div className='skillBarText'>
+                <h2>Website Design</h2>
+                <p>This demo text can be changed while making the production ready website.</p>
+            </div>
+</div>
+             <div className='skillBar'>
+            <img src={AppDesign} alt='AppDesign' className='skillBarImg'/>
+            <div className='skillBarText'>
+                <h2>App Design</h2>
+                <p>You can write text related to mobile app development.</p>
+            </div>
+                
+            
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Skills;
